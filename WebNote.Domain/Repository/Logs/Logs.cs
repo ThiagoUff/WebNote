175e40b85bc988b85cfc97fecc0b3e2989728014
João Vítor <32188614+JoaovitorFerreira@users.noreply.github.com>
2022-06-23 09:16:22 -0300

    criacao de response de ML e inclusao de log

---
+++ WebNote.Domain/Repository/Logs/Logs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebNote.Domain.Repository.Logs
+{
+    public class Logs
+    {
+        public string Id { get; set; } = null!;
+        public string Username { get; set; } = null!;
+        public string LogText { get; set; } = null!;
+        public DateTime LogedOn { get; set; }
+    }
+}
